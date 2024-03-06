7b0a48ad394aabb1d068a22f0a5e7023aa814a42
avilagabriella <avilagabriella84@gmail.com>
2024-03-06 09:24:24 -0300

    Atts

--- api/src/controllers/cliente.js
+++ api/src/controllers/cliente.js
@@ -2,16 +2,18 @@ const con = require('../connection/mysql');
 
 //CRUD - CREATE
 const addCliente = (req, res) => {
-    try {
+    if(req.body!=null && req.body.nome!=null && req.body.endereco!=null && req.body.telefone!=null && req.body.email!=null){
         const { nome, endereco, telefone, email } = req.body;
         con.query('INSERT INTO cliente (nome, endereco, telefone, email) VALUES (?, ?, ?, ?)', [nome, endereco, telefone, email], (err, result) => {
             if (err) {
-                res.status(500).send('Erro ao adicionar cliente');
+                res.status(500).json('Erro ao adicionar cliente');
+            }else{
+                req.body.id = result.insertId;
+                res.status(201).json(req.body);
             }
-            res.status(201).send('Cliente adicionado com sucesso');
         });
-    } catch (error) {
-        res.status(500).send('Erro ao adicionar cliente');
+    } else {
+        res.status(400).json('Favor enviar todos os campos obrigatórios');
     }
 }
 
--- api/src/routes.js
+++ api/src/routes.js
@@ -1,18 +1,16 @@
 const express = require('express');
-
-
 const routes = express.Router();
 
-
 const Cliente = require('./controllers/cliente');
+const Veiculo = require('./controllers/veiculo');
 
-routes.get('/', (req,res) => {
-    res.json("API Transportadora ACME 1.0")
+routes.get('/', (req, res) => {
+    res.json("API Transportadora XPTO 1.0")
 });
 
-routes.post('/clientes', Cliente.addClintes);
-routes.get('/clientes', Cliente.getClintes);
+routes.post('/clientes', Cliente.addCliente);
+routes.get('/clientes', Cliente.getClientes);
 
-
+routes.post('/veiculos', Veiculo.addVeiculo);
 
 module.exports = routes;
